bfae9d287f67cdbeb81e0b364ab6a4e6d451cd9d
Michael Bayha <kg7pnh@arrl.net>
2022-02-18 22:26:10 -0800

    extended app based on assignment instructions

--- app.js
+++ app.js
@@ -8,21 +8,21 @@ var app = express();
 
 
 const movies = [
-  {title: 'Wild At Heart', year: '1993' },
-  {title: 'Moonstruck', year: '1994' },
-  {title: 'Raising Arizona', year: '1995' }
+    { title: 'Wild At Heart', year: '1993' },
+    { title: 'Moonstruck', year: '1994' },
+    { title: 'Raising Arizona', year: '1995' }
 ]
 
 const posts = [
-  {title: 'Title 1', body: 'Body 1' },
-  {title: 'Title 2', body: 'Body 2' },
-  {title: 'Title 3', body: 'Body 3' },
-  {title: 'Title 4', body: 'Body 4' },
+    { title: 'Title 1', body: 'Body 1' },
+    { title: 'Title 2', body: 'Body 2' },
+    { title: 'Title 3', body: 'Body 3' },
+    { title: 'Title 4', body: 'Body 4' },
 ]
 const user = {
-  firstName: 'Good',
-  lastName: 'Student',
-  admin: true
+    firstName: 'Good',
+    lastName: 'Student',
+    admin: true
 }
 
 // view engine setup
@@ -36,38 +36,44 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('pages/index', {
-      user
-  })
+    res.render('pages/index', {
+        user
+    })
 })
 
 app.get('/about', (req, res) => {
-  res.render('pages/about', {
-      user
-  })
+    res.render('pages/about', {
+        user
+    })
 })
 
 app.get('/articles', (req, res) => {
-  res.render('pages/articles', {
-      articles: posts
-  })
+    res.render('pages/articles', {
+        articles: posts
+    })
+})
+
+app.get('/movies', (req, res) => {
+    res.render('pages/movies', {
+        movies: movies
+    })
 })
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('pages/error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('pages/error');
 });
 
 module.exports = app;
